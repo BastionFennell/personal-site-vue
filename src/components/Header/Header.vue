8ae84053762f818626d5b5f71dcf40d88cbfa6e9
Bastion Fennell <fennell@csdisco.com>
2019-03-15 22:40:53 -0500

    Add a caret to make it clear you can scroll down

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="header">
     <BastionTheX class="bastion-the-x" />
+    <div class="caret-bounce">
+      <div class="caret" />
+    </div>
   </div>
 </template>
 
@@ -24,6 +27,7 @@ export default class Header extends Vue {}
   box-sizing: border-box;
   height: 100vh;
   padding-top: calc(50vh - 3em);
+  position: relative;
   text-align: center;
 }
 
@@ -37,6 +41,75 @@ export default class Header extends Vue {}
   left: 0;
   position: absolute;
   z-index: 1;
+}
+
+i {
+  bottom: 150px;
+  color: @CYAN9;
+  height: 3em;
+  left: 50%;
+  position: absolute;
+  width: 3em;
+}
+
+.caret-bounce {
+  animation-delay: 7s;
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+  animation-name: caret-bounce;
+  bottom: 100px;
+  left: 50%;
+  position: absolute;
+}
+
+.caret {
+  animation-delay: 5s;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-name: caret;
+  opacity: 0;
+  position: relative;
+}
+
+.caret:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-top: 30px solid @CYAN9;
+  border-left: 30px solid transparent;
+  border-right: 30px solid transparent;
+}
+
+.caret:after {
+  content: '';
+  position: absolute;
+  left: 2px;
+  top: 0;
+  border-top: 28px solid #fff;
+  border-left: 28px solid transparent;
+  border-right: 28px solid transparent;
+}
+
+@keyframes caret {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes caret-bounce {
+  0% {
+    bottom: 100px;
+  }
+  50% {
+    bottom: 60px;
+  }
+  100% {
+    bottom: 100px;
+  }
 }
 
 </style>
